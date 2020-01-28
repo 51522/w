4cb4ec21ee9fba6fd3d2761608715bd1e826bbe9
51522 <58913891+51522@users.noreply.github.com>
2020-01-28 20:41:08 +0300

    Update unblock.sh

--- unblock.sh
+++ unblock.sh
@@ -2,7 +2,7 @@
 
 logger "start update of unblock"
 domains=/tmp/domains.$$
-dnsmasqcfg=/etc/storage/unblock.dnsmasq
+dnsmasqcfg=/tmp/unblock.dnsmasq
 dnsmasqtmp=$dnsmasqcfg.$$
 
 escape(){
@@ -11,8 +11,10 @@ escape(){
 trap escape EXIT
 echo "# Tor check
 check.torproject.org
+
 # Торрент-трекеры
 rutracker.org
+rutor.org
 rutor.info
 rutor.is
 mega-tor.org
@@ -34,6 +36,7 @@ rustorka.net
 uniongang.tv
 fast-torrent.ru
 hdrezka.download
+
 # Каталоги медиаконтента для программ
 720-hd-online.com
 1001kniga.download
@@ -76,6 +79,7 @@ ipleer.top
 ivbox.me
 kinoclub.me
 kinogo-go.com
+kinogo.by
 livefootball.su
 lost-film-hd.world
 lux-film.net
@@ -91,6 +95,7 @@ filmix.net
 filmix.me
 seasonvar.ru
 kinozoro.net
+
 # Книги
 all-ebooks.com
 audiobooks.pw
@@ -101,12 +106,14 @@ flibusta.is
 flibs.me
 flisland.net
 flibusta.site
+
 # Разное
 7-zip.org
 edem.tv
 lurkmore.co
 4pna.com
 2019.vote
+
 # Телеграм
 api.telegram.org
 telegram.org
@@ -126,25 +133,52 @@ venus.web.telegram.org
 flora.web.telegram.org
 vesta.web.telegram.org
 pluto.web.telegram.org
-aurora.web.telegram.org" >> $domains || exit
+aurora.web.telegram.org
+149.154.160.0/20
+91.108.4.0/22
+91.108.8.0/22
+91.108.12.0/22
+91.108.16.0/22
+91.108.56.0/22
+109.239.140.0/24
+67.198.55.0/24" >> $domains || exit
+
+modprobe ip_set
 modprobe ip_set_hash_ip
 modprobe ip_set_hash_net
+modprobe ip_set_bitmap_ip
+modprobe ip_set_list_set
 modprobe xt_set
 ipset create unblock hash:net
 ipset flush unblock
+
 while read line || [ -n "$line" ]; do
   [ -z "$line" ] && continue
   [ "${line:0:1}" = "#" ] && continue
   cidr=$(echo $line | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}')
-  [ ! -z "$cidr" ] && continue
-  ipset -exist add unblock $cidr
+
+  if [ ! -z "$cidr" ]; then
+    ipset -exist add unblock $cidr
+    continue
+  fi
+    
   range=$(echo $line | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}-[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-  [ ! -z "$range" ] && continue
-  ipset -exist add unblock $range 
+
+  if [ ! -z "$range" ]; then
+    ipset -exist add unblock $range
+    continue
+  fi
+    
   addr=$(echo $line | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-  [ ! -z "$addr" ] && continue
-  ipset -exist add unblock $addr
+
+  if [ ! -z "$addr" ]; then
+    ipset -exist add unblock $addr
+    continue
+  fi
+    
+  nslookup $line 1.1.1.1 | grep -v '1.1.1.1' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{system("ipset -exist add unblock "$1)}'
 done < $domains || exit
+
 while read line || [ -n "$line" ]; do
   [ -z "$line" ] && continue
   [ "${line:0:1}" = "#" ] && continue
@@ -152,8 +186,7 @@ while read line || [ -n "$line" ]; do
   echo "ipset=/$line/unblock" >> $dnsmasqtmp
 done < $domains || exit
 echo "ipset=/onion/unblock" >> $dnsmasqtmp
-echo "server=/onion/127.0.0.1#9053" >> $dnsmasqtmp
 mv -f $dnsmasqtmp $dnsmasqcfg
 restart_dhcpd
 restart_firewall
-logger "update unblock was finished"
+logger "update of unblock was finished"
