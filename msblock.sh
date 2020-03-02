2afb90f4f5c6966e8900ee76edd755e90374ce22
51522 <58913891+51522@users.noreply.github.com>
2020-03-02 22:19:29 +0300

    Update msblock.sh

--- msblock.sh
+++ msblock.sh
@@ -3,7 +3,7 @@
 logger "download msblock list"
 cat /dev/null > /etc/storage/msblock
 echo Download List
-wget -q -O- https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt | grep -v "^#" | cut -d "#" -f 1 | sed 's/127\.0\.0\.1/0\.0\.0\.0/' | grep "^0.0.0.0" | sed 's/  */ /g' | sed 's/\t/ /g' |sed 's/\r//' | cut -d " " -f 1,2 | tr A-Z a-z | sort | uniq > /etc/storage/msblock
+wget -q -O- https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt | grep -v "^#" | cut -d "#" -f 1 | sed 's/127\.0\.0\.1/0\.0\.0\.0/' | grep "^127.0.0.1" | sed 's/  */ /g' | sed 's/\t/ /g' |sed 's/\r//' | cut -d " " -f 1,2 | tr A-Z a-z | sort | uniq > /etc/storage/msblock
 
 echo Generation Block List
 echo Restart dnsmasq
